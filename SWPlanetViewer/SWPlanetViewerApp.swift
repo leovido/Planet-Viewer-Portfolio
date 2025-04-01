8f6d2c37518b5dfc9223b9b6b15d017cd395676c
Christian Ray Leovido <18484997+leovido@users.noreply.github.com>
2025-04-01 11:14:00 +0200

    Initial Commit

---
+++ SWPlanetViewer/SWPlanetViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWPlanetViewerApp.swift
+//  SWPlanetViewer
+//
+//  Created by Christian Ray Leovido on 01/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct SWPlanetViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
